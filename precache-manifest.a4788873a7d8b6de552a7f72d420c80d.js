c59526b791eb7713e022d1380934015701268acf
fcastrogarcia <fede.castrog@gmail.com>
2019-03-01 20:47:36 -0300

    Updates

---
+++ precache-manifest.a4788873a7d8b6de552a7f72d420c80d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "338a5edb97d90d856075",
+    "url": "/new-app/static/js/runtime~main.338a5edb.js"
+  },
+  {
+    "revision": "d647be211caab43de57a",
+    "url": "/new-app/static/js/main.d647be21.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/new-app/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "d647be211caab43de57a",
+    "url": "/new-app/static/css/main.60464f06.chunk.css"
+  },
+  {
+    "revision": "79a9d84b1e0d3ad8a0e99ac95d7b9b7b",
+    "url": "/new-app/index.html"
+  }
+];
