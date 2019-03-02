e1fe0a83b0224bb61c6574579e5434025b065616
fcastrogarcia <fede.castrog@gmail.com>
2019-03-02 12:40:52 -0300

    several fixes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Buttons from './Buttons';
 import Display from './Display';
+import * as math from 'mathjs';
 
 
 const isOperator = /[\+\-\/\*]/;
@@ -12,22 +13,27 @@ class App extends React.Component {
     super(props);
     this.state = {
       currVal: '',
-      formula: '0'
+      formula: '0',
+      evaluated: false
     }
  this.handleInput = this.handleInput.bind(this);
  this.handleOperators = this.handleOperators.bind(this);
  this.handleResult = this.handleResult.bind(this);
  this.handleDecimal = this.handleDecimal.bind(this);
  this.initialize = this.initialize.bind(this);
+ this.handleErase = this.handleErase.bind(this);
+
   }
 
   handleInput(e) {
     this.setState({
       currVal: this.state.currVal == '0' || isOperator.test(this.state.currVal) ? e.target.value :
                this.state.currVal + e.target.value,
-      formula: this.state.currVal == '0' && e.target.value == '0' ? this.state.formula:
-               this.state.formula == '0' ? e.target.value:
+      formula: this.state.currVal == '0' && e.target.value == '0' ? this.state.formula :
+               this.state.formula == '0' ? e.target.value :
+               this.state.evaluated==true ? e.target.value :
                this.state.formula + e.target.value,
+    evaluated: false
     })
   }
 
@@ -36,15 +42,19 @@ class App extends React.Component {
       currVal: e.target.value,
       formula: this.state.formula[this.state.formula.length-1] == '.' ? this.state.formula.slice(0,-1) + e.target.value :
                !isOperator.test(this.state.currVal) ? this.state.formula + e.target.value :
-                this.state.formula.slice(0,-1) + e.target.value
+               this.state.formula.slice(0,-1) + e.target.value,
+    evaluated: false
     })
     }
 
   handleResult() {
-    let result = eval(this.state.formula).toString();
+    let result = math.eval(this.state.formula).toString();
     this.setState({
-      formula: result.includes('.') && result.length > 10 ? result.slice(0,11) : result,
-      currVal: ''
+      formula: result.includes('.') && result.length > 10 ? result.slice(0,11) :
+               isOperator.test(this.state.currVal) ? this.state.formula :
+               result,
+      currVal: '',
+      evaluated: true
     })
   }
 
@@ -58,35 +68,35 @@ class App extends React.Component {
                this.state.currVal + e.target.value,
       formula: numberAfterLastOperator == null ? this.state.formula + '0.' :
                numberAfterLastOperator[0].includes('.') ? this.state.formula :
-               this.state.formula + e.target.value
+               this.state.formula + e.target.value,
+    evaluated: false
     })
   }
- initialize(e) {
-   switch(e.target.value) {
-    case "AC":
+ initialize() {
     this.setState({
         currVal: '',
-        formula: '0'
+        formula: '0',
+        evaluated: false
     })
-    break;
-    case "erase":
+  }
+
+  handleErase = () => {
     this.setState({
       currVal: this.state.currVal=='0' ? this.state.currVal :
                this.state.currVal.length==1 ? '0'  :
                this.state.currVal.slice(0,-1),
       formula: this.state.formula=='0' ? this.state.formula :
                this.state.formula.length==1 ? '0' :
-               this.state.formula.slice(0,-1)
+               this.state.formula.slice(0,-1),
+    evaluated: false
     })
-    break;
-  }
   }
 
   render () {
     return (
     <div id="container">
         <Display currVal={this.state.currVal} formula={this.state.formula}/>
-        <Buttons passValue={this.handleInput} initialize={this.initialize} handleOperators={this.handleOperators} result={this.handleResult} handleDecimal={this.handleDecimal}/>
+        <Buttons handleInput={this.handleInput} initialize={this.initialize} handleOperators={this.handleOperators} result={this.handleResult} handleDecimal={this.handleDecimal} handleErase={this.handleErase}/>
      </div>
     )
   }
--- src/Buttons.js
+++ src/Buttons.js
@@ -5,22 +5,22 @@ class Buttons extends React.Component {
     return (
     <div className="buttons-container">
         <button id="clear" value='AC' onClick={this.props.initialize}>C</button>
-        <button id="erase" value="erase" onClick={this.props.initialize}><i class="fas fa-backspace"></i></button>
+        <button id="erase" value="erase" onClick={this.props.handleErase}><i class="fas fa-backspace"></i></button>
         <button id="multiply" value='*' onClick={this.props.handleOperators}>x</button>
         <button id="divide" value='/' onClick={this.props.handleOperators}>/</button>
-        <button id="one" value='1' onClick={this.props.passValue}>1</button>
-        <button id="two" value='2' onClick={this.props.passValue}>2</button>
-        <button id="three" value='3' onClick={this.props.passValue}>3</button>
+        <button id="one" value='1' onClick={this.props.handleInput}>1</button>
+        <button id="two" value='2' onClick={this.props.handleInput}>2</button>
+        <button id="three" value='3' onClick={this.props.handleInput}>3</button>
         <button id="add" value='+' onClick={this.props.handleOperators}>+</button>
-        <button id="four" value='4' onClick={this.props.passValue}>4</button>
-        <button id="five" value='5' onClick={this.props.passValue}>5</button>
-        <button id="six" value='6' onClick={this.props.passValue}>6</button>
+        <button id="four" value='4' onClick={this.props.handleInput}>4</button>
+        <button id="five" value='5' onClick={this.props.handleInput}>5</button>
+        <button id="six" value='6' onClick={this.props.handleInput}>6</button>
         <button id="subtract" value='-' onClick={this.props.handleOperators}>-</button>
-        <button id="seven" value='7' onClick={this.props.passValue}>7</button>
-        <button id="eight" value='8' onClick={this.props.passValue}>8</button>
-        <button id="nine" value='9' onClick={this.props.passValue}>9</button>
+        <button id="seven" value='7' onClick={this.props.handleInput}>7</button>
+        <button id="eight" value='8' onClick={this.props.handleInput}>8</button>
+        <button id="nine" value='9' onClick={this.props.handleInput}>9</button>
         <button id="equals" onClick={this.props.result}>=</button>
-        <button id="zero" value='0' onClick={this.props.passValue}>0</button>
+        <button id="zero" value='0' onClick={this.props.handleInput}>0</button>
         <button id="decimal" value='.' onClick={this.props.handleDecimal}>.</button>
     </div>
     )
